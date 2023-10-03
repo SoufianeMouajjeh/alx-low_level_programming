0a92fd1590abc5ce935d2dc4aac628bebd9fc8c1
SoufianeMouajjeh <melmouajjeh@gmail.com>
2023-10-03 11:14:25 -0700

    done

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,14 +14,12 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	for (j = 0; str[j] != '\0'; j++)
-	 	;
+	for (j = 0; str[j] != '\0'; j++);
 	s = (char*)malloc(sizeof(char) * (j + 1));
-        if (s == NULL){
-	     return (NULL);
-	}
-        for (i = 0; i < j; i++)
-		 s[i] = str[i];
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < j; i++)
+		s[i] = str[i];
 	s[j] ='\0';
 	return(s);
 }	     
