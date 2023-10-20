86e9a90f8344ac07736aae00249c78e6fc115b0f
SoufianeMouajjeh <melmouajjeh@gmail.com>
2023-10-20 16:30:52 -0700

    betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,19 +12,20 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t node_count = 0;
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-        {
-            printf("[0] (nil)\n");
-        }
-        else
-        {
-            printf("[%d] %s\n", h->len, h->str);
-        }
-        h = h->next;
-        node_count++;
-    }
-    return node_count;
+	size_t node_count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		node_count++;
+	}
+	return (node_count);
 }
