908b02048fc3eafa94aaa1361e504a0d2deb04ac
Soufiane El mouajjeh <137649810+SoufianeMouajjeh@users.noreply.github.com>
2023-10-20 23:44:26 +0000

    done

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include"lists.h"
+/**
+ * pre_main_message - Prints a pre-main message.
+ */
+void printTextBeforeMain()  
+{
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+
+int main()
+{
+    printTextBeforMain();
+    printf("Main function has been executed.\n")
+    return (0);
+}
