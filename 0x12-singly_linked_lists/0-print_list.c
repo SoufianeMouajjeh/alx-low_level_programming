ef5a3971f48e281f6b7e7cedf306ef140c19ea0f
Soufiane El mouajjeh <137649810+SoufianeMouajjeh@users.noreply.github.com>
2023-10-20 23:22:10 +0000

    issue

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,11 +10,11 @@
  *
  * Return: The number of nodes.
  */
-size_t print_list(const list_t *h)  
+size_t print_list(const list_t *h)
 {
     size_t node_count = 0;
     while (h != NULL) {
-        if (h->str == NULL) 
+        if (h->str == NULL)     
         {
             printf("[0] (nil)\n");
         }   
@@ -22,7 +22,6 @@ size_t print_list(const list_t *h)
         {
             printf("[%d] %s\n", h->len, h->str);
         }
-
         h = h->next;
         node_count++;
     }
