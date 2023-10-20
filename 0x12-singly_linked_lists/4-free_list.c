ccbe3b75497142b6a4ff307a0f570bab14d584b6
Soufiane El mouajjeh <137649810+SoufianeMouajjeh@users.noreply.github.com>
2023-10-20 23:11:17 +0000

    solving

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,11 +6,12 @@
  * @head: A pointer to the head of the list.
  */
 void free_list(list_t *head){
+    int free;
     if (head == NULL){
         return;
     }
 }
-free_list(head->next);
+    free_list(head->next);
 if (head->str != NULL)
 {
     free(head->str);
