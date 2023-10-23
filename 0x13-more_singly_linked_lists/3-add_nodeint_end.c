86ad0da08ffc5c7b7fae62b29f2c76c83fa59739
Soufiane El mouajjeh <137649810+SoufianeMouajjeh@users.noreply.github.com>
2023-10-23 14:45:50 +0000

    done

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -30,7 +30,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
     else
     {
         current = *head;
-        while (current->next != NULL);
+        while (current->next != NULL)
         {
             current = current->next;
         }
