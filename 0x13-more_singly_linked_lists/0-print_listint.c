57d0b07ec7cd9f8706cd4395ebde0d0add0cfacc
Soufiane El mouajjeh <137649810+SoufianeMouajjeh@users.noreply.github.com>
2023-10-23 12:55:16 +0000

    solveing

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,7 @@
-#include<stdio.h>
-#include "lists.h"/**
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
  /**
  * print_list - Prints all the elements of a list_t list.
  * @h: A pointer to the head of the list.
