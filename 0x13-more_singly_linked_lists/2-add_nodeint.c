6b26b72fd57323ee26871f2128eb3944a18caa50
Soufiane El mouajjeh <137649810+SoufianeMouajjeh@users.noreply.github.com>
2023-10-23 14:19:29 +0000

    add new node

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - adds a new node at the beginning of a list .
+ * @h: A pointer to the head of the list.
+ *
+ * Return: The number of nodes.
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)  
+{
+    listint_t *new_node;
+    *new_node = malloc(sizeof(listint_t));
+    if(new_node == NULL)    
+    {
+        return (NULL);
+    }
+    new_node->n = n;
+    new_node->next = *head;
+    *head =new_node;
+
+    return new_node;
+}
