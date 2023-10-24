d7ad1e35696fc49a388601c95a0dfbd806989fa2
Soufiane El mouajjeh <137649810+SoufianeMouajjeh@users.noreply.github.com>
2023-10-24 19:40:50 +0000

    solving 2 variabls

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -13,14 +13,13 @@
 listint_t *reverse_listint(listint_t **head)
 {
     listint_t *prev = NULL, *next;
-    listint_t *current = *head;
 
-    while (current != NULL)
+    while (*head != NULL)
     {
-        next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
+        next = (*head)->next;
+        (*head)->next = prev;
+        prev = *head;
+        *head = next;
     }
     *head = prev;
     return (prev);
