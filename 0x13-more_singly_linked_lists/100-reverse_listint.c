dafb7b76c0686c2129bf7435e84e7dc02d655d89
Soufiane El mouajjeh <137649810+SoufianeMouajjeh@users.noreply.github.com>
2023-10-24 19:29:46 +0000

    done

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -14,10 +14,11 @@ listint_t *reverse_listint(listint_t **head)
 {
     listint_t *prev = NULL;
     listint_t *current = *head;
-    listint_t *next = current->next;
+    listint_t *next;
 
     while (current != NULL)
     {
+        next = current->next
         current->next = prev;
         prev = current;
         current = next;
