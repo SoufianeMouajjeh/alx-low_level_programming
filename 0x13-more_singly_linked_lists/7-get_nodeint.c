c6bae24cb206b95dc20ee33a6983d08f1b63abf0
SoufianeMouajjeh <melmouajjeh@gmail.com>
2023-10-25 10:58:03 -0700

    fixed betty

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,27 +2,33 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
 /**
- * print_list - returns the nth node of a list .
- * @h: A pointer to the head of the list.
+ * get_nodeint_at_index - Returns the nth node of a listint_t list.
+ * @head: A pointer to the head of the list.
+ * @index: The index of the node to retrieve.
  *
- * Return: the nth node of list.
+ * Description:
+ * This function retrieves the node at a specific index in a `listint_t`
+ * linked list. The index is zero-based, where 0 refers to the head node.
+ * It iterates through the list and returns a pointer to the desired node.
+ *
+ * Return:
+ * If the node is found, a pointer to the node; otherwise, NULL if the
+ * node at the given index does not exist.
  */
-
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)    
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-    unsigned int current_index = 0;
-    listint_t *current = head;
+	unsigned int current_index = 0;
+	listint_t *current = head;
 
-    while (current != NULL)
-    {
-        if (current_index == index)
-        {
-            return current;
-        }
-        current = current->next;
-        current_index++;
-    }
-    return NULL;
+	while (current != NULL)
+	{
+		if (current_index == index)
+		{
+			return (current);
+		}
+		current = current->next;
+		current_index++;
+	}
+	return (NULL);
 }
