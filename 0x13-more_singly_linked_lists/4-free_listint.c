ed1e83e2e19524023a30884d06c798132586783a
SoufianeMouajjeh <melmouajjeh@gmail.com>
2023-10-25 10:10:14 -0700

    fixed betty

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,22 +2,27 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
 /**
- * free_list - free the list .
- * @h: A pointer to the head of the list.
+ * free_listint - Frees a listint_t list.
+ * @head: A pointer to the head of the list.
  *
- * Return: free current.
+ * Description:
+ * This function takes a pointer to the head of a `listint_t` linked list and
+ * frees all the memory allocated for the nodes in the list. It iterates
+ * through the list, releases the memory for each node, and sets the head
+ * pointer to NULL.
+ *
+ * Return: void (no return value)
  */
 
 void free_listint(listint_t *head)
 {
-    listint_t *current;
+	listint_t *current;
 
-    while(head != NULL) 
-    {
-        current = head;
-        head = head->next;
-        free(current);
-    }
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+	free(current);
+	}
 }
