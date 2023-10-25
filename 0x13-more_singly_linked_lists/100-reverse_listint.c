9e6086ffb7a21def49e175fc536e0fc8f171b640
SoufianeMouajjeh <melmouajjeh@gmail.com>
2023-10-25 11:27:26 -0700

    fixed betty

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -17,15 +17,15 @@
 
 listint_t *reverse_listint(listint_t **head)
 {
-    listint_t *prev = NULL, *next;
+	listint_t *prev = NULL, *next;
 
-    while (*head != NULL)
-    {
-        next = (*head)->next;
-        (*head)->next = prev;
-        prev = *head;
-        *head = next;
-    }
-    *head = prev;
-    return (prev);
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+	*head = prev;
+	return (prev);
 }
