9490a762eaf6d81158b9b4c9ae51042fa74bd417
SoufianeMouajjeh <melmouajjeh@gmail.com>
2023-10-25 11:52:12 -0700

    fixed betty

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -34,7 +34,7 @@ size_t free_listint_safe(listint_t **h)
 			break;
 		}
 	}
-	*h = NULL;
 
+	*h = NULL;
 	return (size);
 }
--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -17,23 +17,23 @@
 
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *tortoise = head;
-    listint_t *hare = head;
+	listint_t *tortoise = head;
+	listint_t *hare = head;
 
-    while (tortoise != NULL && hare != NULL && hare->next != NULL)  
-    {
-        tortoise = tortoise->next;
-        hare = hare->next->next;
-        if (tortoise == hare)   
-        {
-            tortoise = head;
-            while (tortoise != hare)   
-            {
-                tortoise = tortoise->next;
-                hare = hare->next;
-            }
-            return hare;
-        }
-    }
-    return NULL;
+	while (tortoise != NULL && hare != NULL && hare->next != NULL)
+	{
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			return (hare);
+		}
+	}
+	return (NULL);
 }
