7a64abbb7e6beda175f812a79620b6ce05a9c68d
SoufianeMouajjeh <melmouajjeh@gmail.com>
2023-10-25 05:38:36 -0700

    fixed betty

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,7 +7,7 @@
  * print_list - Prints all the elements of a list_t list
  * @h: A pointer to the head of the list
  *
- * Return: The number of nodes	
+ * Return: The number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
