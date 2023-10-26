297db86c4dfbeb5612e5218611b7f821006f3a3a
Soufiane El mouajjeh <137649810+SoufianeMouajjeh@users.noreply.github.com>
2023-10-26 22:54:12 +0000

    fixed betty

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -7,13 +7,16 @@
  *
  * Return: 1 if it worked, or -1 if an error occurred.
  */
-int clear_bit(unsigned long int *n, unsigned int index)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    if (index >= sizeof(unsigned long int) * 8)
+    unsigned long int xor_result = n ^ m;
+    unsigned int count = 0;
+
+    while (xor_result)
     {
-        return (-1);
+        count += xor_result & 1;
+        xor_result >>= 1;
     }
-    *n &= ~(1UL << index);
-    return (1);
+    return count;
 }
 
