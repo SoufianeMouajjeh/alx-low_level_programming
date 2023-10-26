864ea1b6584efe46f9d675ec26f9b5c5b7eb2c24
Soufiane El mouajjeh <137649810+SoufianeMouajjeh@users.noreply.github.com>
2023-10-26 22:46:37 +0000

    done

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * clear_bit - Sets the value of a bit to 0 at a given index.
+ * @n: A pointer to the unsigned long integer.
+ * @index: The index of the bit to be cleared (starting from 0).
+ *
+ * Return: 1 if it worked, or -1 if an error occurred.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+    if (index >= sizeof(unsigned long int) * 8) 
+    {
+        return (-1);  
+    }
+    *n &= ~(1UL << index);
+    return (1);
+}
+
+
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -24,5 +24,20 @@ void print_binary(unsigned long int n);
  * Return: The value of the bit at index index or -1 if an error occurred.
  */
 int get_bit(unsigned long int n, unsigned int index);
-
+/**
+ * set_bit - Sets the value of a bit to 1 at a given index.
+ * @n: A pointer to the unsigned long integer.
+ * @index: The index of the bit to be set to 1 (starting from 0).
+ *
+ * Return: 1 if it worked, or -1 if an error occurred.
+ */
+int set_bit(unsigned long int *n, unsigned int index);
+/**
+ * clear_bit - Sets the value of a bit to 0 at a given index.
+ * @n: A pointer to the unsigned long integer.
+ * @index: The index of the bit to be cleared (starting from 0).
+ *
+ * Return: 1 if it worked, or -1 if an error occurred.
+ */
+int clear_bit(unsigned long int *n, unsigned int index);
 #endif
