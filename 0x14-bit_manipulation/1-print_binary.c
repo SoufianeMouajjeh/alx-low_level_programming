d4280eab1920df9f58d3f4a08503aa94c19eed71
SoufianeMouajjeh <melmouajjeh@gmail.com>
2023-10-27 09:24:49 -0700

    fixed betty

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,9 +7,9 @@
 
 void print_binary(unsigned long int n)
 {
-    if (n > 1)
-    {
-        print_binary(n >> 1);
-    }
-    putchar((n & 1) + '0');
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	putchar((n & 1) + '0');
 }
