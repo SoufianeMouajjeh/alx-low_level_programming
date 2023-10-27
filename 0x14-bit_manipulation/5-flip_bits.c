60fb2a1c245a5071a66b84ae96c0ce18a77a00a1
SoufianeMouajjeh <melmouajjeh@gmail.com>
2023-10-27 09:37:23 -0700

    merge

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * clear_bit - Sets the value of a bit to 0 at a given index.
- * @n: A pointer to the unsigned long integer.
+ * Flips the bits of an integer value..
+ * value (int): The integer value whose bits will be flipped..
  * @index: The index of the bit to be cleared (starting from 0).
  *
- * Return: 1 if it worked, or -1 if an error occurred.
+ * int: The integer with its bits flipped..
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    unsigned long int xor_result = n ^ m;
-    unsigned int count = 0;
+	unsigned long int xor_result = n ^ m;
+	unsigned int count = 0;
 
-    while (xor_result)
-    {
-        count += xor_result & 1;
-        xor_result >>= 1;
-    }
-    return count;
+	while (xor_result)
+	{
+		count += xor_result & 1;
+		xor_result >>= 1;
+	}
+	return (count);
 }
 
