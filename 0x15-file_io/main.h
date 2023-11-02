f0d1313bf66a6c604569310b449cd5ae71e74332
Soufiane El mouajjeh <137649810+SoufianeMouajjeh@users.noreply.github.com>
2023-11-02 14:06:26 +0000

    fixed main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,26 +1,38 @@
-#include <unistd.h>
+#ifndef MAIN_H
+#define MAIN_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <elf.h>
+#include <string.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * read_textfile - Reads a text file and prints it to standard output.
- * @filename: The name of the file to read.
- * @letters: The number of letters to read and print.
- *
- * Return: The actual number of letters read and printed. 0 on failure.
+ * read_textfile - Reads a text file and prints it to the POSIX standard output
+ * @filename: The name of the file to read
+ * @letters: The number of letters to read and print
+ * Return: The actual number of letters it could read and print
+ * 0 if the file can't be opened or read, or if write fails or does not
+ * write the expected amount of bytes, or if filename is NULL
  */
 ssize_t read_textfile(const char *filename, size_t letters);
-
+/**
+ * create_file - Creates a file and writes text content to it
+ * @filename: The name of the file to create
+ * @text_content: The text content to write to the file
+ * Return: 1 on success, -1 on failure
+ */
 int create_file(const char *filename, char *text_content);
-
+/**
+ * append_text_to_file - Appends text to the end of a file
+ * @filename: The name of the file
+ * @text_content: The text content to append
+ * Return: 1 on success, -1 on failure
+ */
 int append_text_to_file(const char *filename, char *text_content);
+
+
+#endif
