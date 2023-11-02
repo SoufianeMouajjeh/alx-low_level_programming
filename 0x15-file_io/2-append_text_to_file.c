0cef21a28354b4a3d6f27b316566da291814e297
Soufiane El mouajjeh <137649810+SoufianeMouajjeh@users.noreply.github.com>
2023-11-02 14:19:56 +0000

    fixed betty

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,6 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
     if (filename == NULL)
     {
         return (-1);
+    }
     if (text_content == NULL)
     {
         return (1);
@@ -36,6 +37,6 @@ int append_text_to_file(const char *filename, char *text_content)
     }
 
     fclose(file);
-    }
+    
     return (1);
 }
