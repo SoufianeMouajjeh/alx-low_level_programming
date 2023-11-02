c819ff0ac89528b7cf4b3042d17405ea7c543319
Soufiane El mouajjeh <137649810+SoufianeMouajjeh@users.noreply.github.com>
2023-11-02 14:18:22 +0000

    fixed betty

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,25 +12,30 @@ int append_text_to_file(const char *filename, char *text_content)
     size_t bytes_written;
     FILE *file;
 
-    if (filename == NULL) {
+    if (filename == NULL)
+    {
         return (-1);
-    if (text_content == NULL) {
+    if (text_content == NULL)
+    {
         return (1);
     }
 
     file = fopen(filename, "a");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         return (-1);
     }
 
     text_length = strlen(text_content);
     bytes_written = fwrite(text_content, 1, text_length, file);
 
-    if (bytes_written != text_length) {
+    if (bytes_written != text_length)
+    {
         fclose(file);
         return (-1);
     }
 
     fclose(file);
+    }
     return (1);
 }
