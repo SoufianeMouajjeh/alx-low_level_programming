5d542637a50e6a4c46513145f3779d66fe1c77c0
Soufiane El mouajjeh <137649810+SoufianeMouajjeh@users.noreply.github.com>
2023-11-02 14:03:50 +0000

    declaration

--- 0x15-file_io/0-main.c
+++ 0x15-file_io/0-main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "main.h"
 
+
 /**
  * main - check the code
  *
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,3 +11,16 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+/**
+ * read_textfile - Reads a text file and prints it to standard output.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters to read and print.
+ *
+ * Return: The actual number of letters read and printed. 0 on failure.
+ */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+int create_file(const char *filename, char *text_content);
+
+int append_text_to_file(const char *filename, char *text_content);
